44e7b5fd6a23157237b4a8a2ddda328f0a745b35
Mio Engel <mioengel@hotmail.com>
2013-07-02 23:34:08 +0200

    Added On Spawn Support

--- src/me/frodenkvist/scoreboardmanager/HealthListener.java
+++ src/me/frodenkvist/scoreboardmanager/HealthListener.java
@@ -3,6 +3,7 @@ package me.frodenkvist.scoreboardmanager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import Event.PBEntityDamageEntityEvent;
 import Event.PBEntityRegainHealthEvent;
@@ -48,5 +49,18 @@ public class HealthListener implements Listener
 		percent = ((double)health / (double)player.getMaxHealth());
 		//Bukkit.broadcastMessage("" + percent);
 		SMHandler.setHealthBar(percent, (Player)event.getEntity());
+	}
+	
+	@EventHandler
+	public void onPlayerRespawnEvent(PlayerRespawnEvent event)
+	{
+		PVPPlayer player = PvpHandler.getPvpPlayer(event.getPlayer());
+		//double oldPercent = 0.0D;
+		double percent = 0.0D;
+		
+		//oldPercent = (player.gethealth() / player.getMaxHealth());
+		percent = (1.0);
+		//Bukkit.broadcastMessage("" + percent);
+		SMHandler.setHealthBar(percent, player.getPlayer());
 	}
 }
